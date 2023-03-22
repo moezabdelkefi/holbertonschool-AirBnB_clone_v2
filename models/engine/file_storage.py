76bbf9707c60bbd0b884a12a7c5018a01587190d
moezabdelkef <moezabdelkefi17@gmail.com>
2023-03-22 12:51:18 +0100

    Delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 import json
 
 
@@ -8,9 +15,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if (cls is not None):
+            lis = {}
+            for i in self.__objects:
+                if (cls == type(self.__objects[i])):
+                    lis[i] = self.__objects[i]
+            return(lis)
+        else:
+            return (self.__objects)
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -27,13 +41,6 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
 
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
@@ -48,3 +55,9 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Deletes an object from __objects"""
+        if obj is not None:
+            key = obj.__class__.__name__ + "." + obj.id
+            self.__objects.pop(key, None)
