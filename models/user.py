a4dff6e96ba71c3c5d3ca9c522831df39e8428e6
moezabdelkef <moezabdelkefi17@gmail.com>
2023-03-25 15:53:56 +0100

    DBStorage - User

--- models/user.py
+++ models/user.py
@@ -4,18 +4,10 @@ from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel, Base):
-    """User class to store user information"""
-
+class User(Base, BaseModel):
     __tablename__ = 'users'
-
+    
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-
-    places = relationship("Place", backref="user", cascade="all, delete")
-
-    def __init__(self, *args, **kwargs):
-        """Initializes User instance"""
-        super().__init__(*args, **kwargs)
