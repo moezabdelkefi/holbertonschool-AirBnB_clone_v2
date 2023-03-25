af4db77f7f4838c2870d993bb1f37c83977e7d3b
moezabdelkef <moezabdelkefi17@gmail.com>
2023-03-25 16:10:35 +0100

    DBStorage - Review

--- models/place.py
+++ models/place.py
@@ -2,21 +2,20 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
-from sqlalchemy.orm import relationship
-
+from sqlalchemy.orm import relationship, backref
 
 class Place(BaseModel, Base):
+    """Representation of a place"""
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024))
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
-
-    user = relationship("User", backref="places", cascade="all, delete")
-    city = relationship("City", backref="places", cascade="all, delete")
+    number_rooms = Column(Integer, default=0)
+    number_bathrooms = Column(Integer, default=0)
+    max_guest = Column(Integer, default=0)
+    price_by_night = Column(Integer, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
+    reviews = relationship("Review", cascade="all, delete-orphan", backref=backref("place", cascade="all, delete"))
