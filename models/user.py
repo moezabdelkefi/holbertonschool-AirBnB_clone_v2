b348916100c445be42176a5c339f38cdfe2af824
moezabdelkef <moezabdelkefi17@gmail.com>
2023-03-25 15:57:22 +0100

    DBStorage - User

--- models/user.py
+++ models/user.py
@@ -11,3 +11,9 @@ class User(Base, BaseModel):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    def __init__(self, email, password, first_name=None, last_name=None):
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
