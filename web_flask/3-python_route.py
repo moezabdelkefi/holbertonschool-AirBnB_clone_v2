c1d5111aa1b370b87956c15e6791e83d819397f6
moezabdelkef <moezabdelkefi17@gmail.com>
2023-04-11 04:12:26 +0100

    a script that starts a Flask web application

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -30,9 +30,10 @@ def c_text(text):
     return 'C {}'.format(text.replace('_', ' '))
 
 
+# @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-@app.roote('/python/<text>', strict_slashes=False)
-def python_text(text):
+def hello2(text='is cool'):
     text = text.replace("_", " ")
     return f"Python {text}"
 
