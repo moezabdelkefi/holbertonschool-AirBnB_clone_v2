25f976b653b488c303171ab7e4e828d514f37999
moezabdelkef <moezabdelkefi17@gmail.com>
2023-04-11 03:45:03 +0100

    a script that starts a Flask web application with /c/<text>

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,7 +20,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """display c followed by the value of the text variable"""
-    return 'c {}'.format(text = text.replace('_', ' '))
+    return 'C {}'.format(text.replace('_', ' '))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
