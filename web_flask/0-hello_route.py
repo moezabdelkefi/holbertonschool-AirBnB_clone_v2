1157b3797048fbebbb77de0a96dfae97dc033f88
moezabdelkef <moezabdelkefi17@gmail.com>
2023-04-11 03:01:17 +0100

    a script that starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,9 @@
-from flask import Flask
+#!/usr/bin/python3
 
+"""
+a script that starts a Flask web application
+"""
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
