cc4f0bdad7575d7ccf3348baa1d74c133357f3e3
moezabdelkef <moezabdelkefi17@gmail.com>
2023-04-12 03:06:49 +0100

    a script that starts a Flask web application

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+"""
+a script that starts a Flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+Routes:
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+/c/<text>: display “C ”, followed by the
+value of the text variable (replace underscore _ symbols with a space )
+/python/<text>: display “Python ”, followed by the value
+of the text variable (replace underscore _ symbols with a space )
+The default value of text is “is cool”
+/number/<n>: display “n is a number” only if n is an integer
+/number_template/<n>: display a HTML page only if n is an integer:
+H1 tag: “Number: n” inside the tag BODY
+/number_odd_or_even/<n>: display a HTML page only if n is an integer:
+H1 tag: “Number: n is even|odd” inside the tag BODY
+"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """display c followed by the value of the text variable"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+# @app.route('/python/', defaults={'text': 'is cool'},strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def hello2(text='is cool'):
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route('/n/<int:n>', strict_slashes=False)
+def is_number(n):
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+@app.route('/number_odd_or_even/<n>', strict_slashes=False)
+def num_add_or_even(n):
+    return render_template('6-number_odd_or_even.html', )
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
