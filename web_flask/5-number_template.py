950fe98de7be821ac5cc86b7edefd2b9d9a3648c
moezabdelkef <moezabdelkefi17@gmail.com>
2023-04-12 03:48:39 +0100

    a script that starts a Flask web application

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -50,7 +50,8 @@ def is_number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return render_template('5-number.html', n=n)
+    if isinstance(n, int):
+        return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
