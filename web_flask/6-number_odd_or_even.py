794edd19458df65326fec6eec1ff0bedbd965ae7
moezabdelkef <moezabdelkefi17@gmail.com>
2023-04-12 03:51:48 +0100

    a script that starts a Flask web application

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -52,7 +52,9 @@ def is_number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return render_template('5-number.html', n=n)
+    if isinstance(n, int):
+        return render_template('5-number.html', n=n)
+
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
